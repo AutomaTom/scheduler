b1b4783b91d3d039a7edced036d4f0333d67c88b
AutomaTom <thomas.waud@gmail.com>
2016-10-01 10:34:38 +0100

    Fixes to Weekday Select
    Couple of small fixes to ensure weekday select works correctly

--- src/aura/CronSelect/CronSelectController.js
+++ src/aura/CronSelect/CronSelectController.js
@@ -1,4 +1,9 @@
 ({
+	init : function(component, event, helper) {
+
+        helper.fireCronUpdateEvent(component);
+	},
+
 	handleChangeEvent : function(component, event, helper) {
         
         if(event.getSource().getLocalId() === "cronSelect"){
